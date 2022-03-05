13cde0b6ebd778c4f7960f9ce3598a68608fd792
Rafael Cauich <85263351+Rafael-C-C@users.noreply.github.com>
2022-03-05 01:50:35 -0600

    Home 5% listo

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -10,12 +10,14 @@ import 'package:gb_shop/Screens/screens.dart';
       return  Scaffold(
           appBar: AppBar(
           backgroundColor: Colors.greenAccent,
-          leading: IconButton(
-          icon: const Icon(Icons.person),
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const LoginScreen(),));
-          },
-        ),
+          actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const LoginScreen(),));
+            },
+          ),
+        ],
           title: Image.asset('assets/TexLogo.png', width: 600, height: 800,),
           elevation: 0,
           centerTitle: true,
