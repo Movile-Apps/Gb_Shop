d1b6b2394e7cfcaa8ef4f2bd2f20d617184bd707
Rafael Cauich <85263351+Rafael-C-C@users.noreply.github.com>
2022-03-27 14:12:56 -0600

    No se que hacer

--- lib/Models/Etiqueta.dart
+++ lib/Models/Etiqueta.dart
@@ -1,3 +1,13 @@
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
+
+import 'dart:convert';
+
+Etiqueta etiquetaFromJson(String str) => Etiqueta.fromJson(json.decode(str));
+
+String etiquetaToJson(Etiqueta data) => json.encode(data.toJson());
+
 class Etiqueta {
     Etiqueta({
         this.exito,
@@ -8,6 +18,18 @@ class Etiqueta {
     int? exito;
     dynamic mensaje;
     List<Datum>? data;
+
+    factory Etiqueta.fromJson(Map<String, dynamic> json) => Etiqueta(
+        exito: json["exito"],
+        mensaje: json["mensaje"],
+        data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "exito": exito,
+        "mensaje": mensaje,
+        "data": List<dynamic>.from(data!.map((x) => x.toJson())),
+    };
 }
 
 class Datum {
@@ -24,6 +46,22 @@ class Datum {
     String? nombre;
     String? descripcion;
     FotoRequest? fotoRequest;
+
+    factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+        idEtiqueta: json["idEtiqueta"],
+        idFoto: json["idFoto"],
+        nombre: json["nombre"],
+        descripcion: json["descripcion"],
+        fotoRequest: FotoRequest.fromJson(json["fotoRequest"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "idEtiqueta": idEtiqueta,
+        "idFoto": idFoto,
+        "nombre": nombre,
+        "descripcion": descripcion,
+        "fotoRequest": fotoRequest!.toJson(),
+    };
 }
 
 class FotoRequest {
@@ -36,4 +74,16 @@ class FotoRequest {
     int? idFoto;
     String? nombre;
     String? url;
+
+    factory FotoRequest.fromJson(Map<String, dynamic> json) => FotoRequest(
+        idFoto: json["idFoto"],
+        nombre: json["nombre"],
+        url: json["url"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "idFoto": idFoto,
+        "nombre": nombre,
+        "url": url,
+    };
 }
